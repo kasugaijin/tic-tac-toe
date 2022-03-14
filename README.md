d3ec8b9144d90e8205d17711f29f193e67d74d51
Ben <robinsonbena@gmail.com>
2022-03-14 16:22:45 -0700

    all working except diagonal check

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tic-tac-toe
+This tic-tac-toe game was written in Ruby and uses a class 'Game' with methods for game logic. Users are prompted to enter their names and then prompted to in turn enter coordinates to select where to place their marker in the grid. If invalid coordinates are entered, the user is prompted to re-enter valid ones. The game announces a winner as soon as there are 3 in a row, or ends with a tie when the grid is full.
--- ruby.rb
+++ ruby.rb
@@ -1,9 +1,7 @@
-# this game has one class, Game
+# one class, Game
 class Game
   @@turn_count = 1
   @@winner = ''
-
-  attr_accessor :player_one_name, :player_two_name
 
   def initialize
     puts 'Player 1 - enter your name!'
@@ -20,43 +18,48 @@ class Game
     puts "#{board[2][0]} | #{board[2][1]} | #{board[2][2]}"
   end
 
-  # check for winners before each turn
-  # repetetive winner and turn count, turn this into a method and repeat that method each time instead.
-  def game_status
-    
-    flat = @board.flatten
-    
-    # check 3 across
-    if @board.each do |i|
+  # check 3 across
+  def three_across  
+    @board.each do |i|
       if i.all? { |j| j == 'X' }
         @@winner = 'X'
-        @@turn_count = 11
+        @@turn_count = 10
       elsif i.all? { |j| j == 'O' }
         @@winner = 'O'
-        @@turn_count = 11
+        @@turn_count = 10
+      else
+        nil
       end
     end
+  end
 
-    # check 3 down
-    elsif flat.each_with_index do |v, i|
+  # check 3 down
+  def three_down
+    flat = @board.flatten
+    flat.each_with_index do |v, i|
       if v == 'X' && flat[i + 3] == 'X' && flat[i + 6] == 'X'
         @@winner = 'X'
-        @@turn_count = 11
+        @@turn_count = 10
       elsif v == 'O' && flat[i + 3] == 'O' && flat[i + 6] == 'O'
         @@winner = 'O'
-        @@turn_count = 11
+        @@turn_count = 10
+      else
+        nil
       end
     end
+  end
 
-    # check diagonal
-    elsif center_val = @board[1][1]
-      if @board[0][0] && @board[2][2] == center_val
-        @@winner = center_val
-        @@turn_count = 11
-      elsif @board[2][0] && @board[0][2] == center_val
-        @@winner = center_val
-        @@turn_count = 11
-      end
+  # check diagonal
+  def three_diagonal
+    center_val = @board[1][1]
+    if @board[0][0] && @board[2][2] == center_val
+      @@winner = center_val
+      @@turn_count = 10
+    elsif @board[2][0] && @board[0][2] == center_val
+      @@winner = center_val
+      @@turn_count = 10
+    else
+      nil
     end
   end
 
@@ -75,9 +78,9 @@ class Game
     coord_one = input_array[0].to_i
     coord_two = input_array[1].to_i
 
-    # until the user includes a space and coords between 0 and 2 for an empty square, prompt for coord
+    # loop until the user input is valid - has space, between 0 and two, board slot is free
     until input.match(/\s/) && coord_one.between?(0, 2) && coord_two.between?(0, 2) && @board[coord_one][coord_two] == '_'
-      puts 'Please enter valid coordinates for an empty space in the grid'
+      puts "Please enter valid coordinates an empty space in the grid"
       input = gets.chomp
       input_array = input.split
       coord_one = input_array[0].to_i
@@ -86,6 +89,7 @@ class Game
 
     add_to_board(coord_one, coord_two, symbol)
   end
+
 
   def add_to_board(coord_one, coord_two, symbol)
     @board[coord_one][coord_two] = symbol
@@ -111,17 +115,19 @@ class Game
     puts "\r\n"
 
     until @@turn_count >= 10 do
-      game_status                           # checks for winner
-      player_turn(@@turn_count)             # evals whose turn it is
-      display_board(@board)                 # displays board each turn
-      puts @@winner
-      puts @@turn_count
+      player_turn(@@turn_count)
+      three_across
+      three_down
+      # three_diagonal
+      display_board(@board)
     end
 
-    declare_result(@symbol)
+    declare_result(@@winner)
   end
+
 end
 
+# instructions
 puts 'Welcome to tic-tac-toe. The rules are as expected, but choosing placement requires coordinates.'
 puts 'Each turn, enter two numbers with a space, per the grid layout below:'
 puts "\r\n"
@@ -130,5 +136,6 @@ puts '1 0 | 1 1 | 1 2'
 puts '2 0 | 2 1 | 2 2'
 puts "\r\n"
 
+# instantiate Game and execute play game
 game = Game.new
 game.play_game
